c2f7f4161ba14f3dda93d822b35c580aba2a5deb
urosh <uros.damnjanovic@gmail.com>
2014-09-04 15:35:10 +0300

    Refactoring controlls directive

--- app/scripts/controlls/controlls.js
+++ app/scripts/controlls/controlls.js
@@ -1,32 +1,20 @@
 'use strict';
 
 
-angular.module('my-controlls', []);
+var myControlls = angular.module('my-controlls', []);
 
 
 
 
-angular.module('my-controlls')
-	.controller('ControllsController', ControllerFunction);
-
-angular.module('my-controlls')
-	.service('ControllsService', ControllsServiceFunction);
-
-angular.module('my-controlls')
-	.directive('selectedItems', SelectedItemsDirective);
 
 
-angular.module('my-controlls')
-	.directive('myDropdown', ControllsDirective);
+myControlls.service('ControllsService', ControllsServiceFunction);
+
+myControlls.directive('selectedTools', SelectedItemsDirective);
+
+
+myControlls.directive('myDropdown', ControllsDirective);
 
 
 
 	
-
-
-// angular.module('controlls')
-// 	.factory('controllFactory', ['', function(){
-// 		return function (){
-			
-// 		};
-// 	}])
