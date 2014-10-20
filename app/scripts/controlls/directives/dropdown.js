10da164c16278c2710449edc5b8aadd988968fab
urosh <uros.damnjanovic@gmail.com>
2014-10-20 13:59:23 +0300

    Fixing dropdown animation

--- app/scripts/controlls/directives/dropdown.js
+++ app/scripts/controlls/directives/dropdown.js
@@ -16,7 +16,7 @@ function DropdownDirective(){
 		controller: DropDownController,
 		require: '^selectedTools',
 		template: '<div class="wrapper-demo">' + 
-		      	  '<div id="dd" ng-class="(active) ? \'wrapper-dropdown active\' : \'wrapper-dropdown \'"  ng-click="active=!active" tabindex="1">' +
+		      	  '<div id="dd" ng-class="(active) ? \'wrapper-dropdown active\' : \'wrapper-dropdown\'"  ng-click="active=!active" tabindex="1">' +
         			'Select tools' +
         			'<ul  class="dropdown">' +
         			'<li ng-repeat="tool in tools"><a href="#" ng-click="select(tool.name)">{{ tool.name }}</a></li>'+
