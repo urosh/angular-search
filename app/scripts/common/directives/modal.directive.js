ff6bef20fcbd279e6a8e135e6711fe4650f4a8b0
urosh <uros.damnjanovic@gmail.com>
2014-10-30 15:30:46 +0200

    Refactoring common directives

--- app/scripts/common/directives/modal.directive.js
+++ app/scripts/common/directives/modal.directive.js
@@ -2,60 +2,72 @@
   'use strict';
 
   angular.module('app.commons')
-    .directive('starcModal', function(){
-      return{
-        restrict: 'A',
-        template: "<div ng-class=\"(vm.loading) ? '' : 'ng-hide'\"><div  ng-class=\"(vm.active) ? 'md-modal md-show' : 'md-modal'\" ng-include='vm.modal' ng-cloak></div><div class='md-overlay' ng-click='vm.removeModal()'></div></div>",
-        controller: ModalController,
-        controllerAs: 'vm'
+    .directive('starcModal', ModalDirective);
 
-      };
-    });
-  ModalController.$inject = ['$scope', 'CommonServices', 'requestNotificationChannel'];
+  function ModalDirective(){
 
-  function ModalController($scope, CommonServices, requestNotificationChannel){
-    var vm = this;
+    var directive = {
+      restrict: 'A',
+      template: "<div ng-class=\"(vm.loading) ? '' : 'ng-hide'\"><div  ng-class=\"(vm.active) ? 'md-modal md-show' : 'md-modal'\" ng-include='vm.modal' ng-cloak></div><div class='md-overlay' ng-click='vm.removeModal()'></div></div>",
+      controller: ModalController,
+      controllerAs: 'vm'
 
-    vm.active = false;
-    vm.imageLocation = '';
-    vm.loading = false;
-    vm.modal = '';
-    vm.metadata = [];
-    vm.title = '';
-    vm.removeModal = removeModal;
+    };
 
 
-    function removeModal() {
+    return directive;
+
+    ModalController.$inject = ['$scope', 'CommonServices', 'requestNotificationChannel'];
+
+    function ModalController($scope, CommonServices, requestNotificationChannel){
+
+      var vm = this;
+
       vm.active = false;
-      vm.title = '';
-      vm.imageLocation = '#';
+      vm.imageLocation = '';
+      vm.loading = false;
+      vm.modal = '';
       vm.metadata = [];
-    }
+      vm.title = '';
+      vm.removeModal = removeModal;
 
 
-
-    function showDetails(docID) {
-      vm.modal = 'scripts/common/templates/modal.tpl.html';
-
-      var result = CommonServices.getItem(docID);
-      result.then(function(res){
+      function removeModal() {
+        vm.active = false;
+        vm.title = '';
+        vm.imageLocation = '#';
         vm.metadata = [];
-        vm.title = res.data.title;
-        vm.imageLocation = res.data.imageLocation;
+      }
 
-        for (var i in res.data.metadataList ){
-          for( var property in res.data.metadataList[i]){
-            vm.metadata.push([property, res.data.metadataList[i][property]]);
+
+
+      function showDetails(docID) {
+        vm.modal = 'scripts/common/templates/modal.tpl.html';
+
+        var result = CommonServices.getItem(docID);
+        result.then(function(res){
+          vm.metadata = [];
+          vm.title = res.data.title;
+          vm.imageLocation = res.data.imageLocation;
+
+          for (var i in res.data.metadataList ){
+            for( var property in res.data.metadataList[i]){
+              vm.metadata.push([property, res.data.metadataList[i][property]]);
+            }
           }
-        }
-        vm.loading = true;
-        vm.active = true;
+          vm.loading = true;
+          vm.active = true;
 
-      });
+        });
+      }
+      requestNotificationChannel.onItemSelected($scope, showDetails);
+
+
     }
-    requestNotificationChannel.onItemSelected($scope, showDetails);
-
   }
+
+
+
 
  })();
 
