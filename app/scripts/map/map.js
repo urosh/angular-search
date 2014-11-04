f89364b78f53a19d19cc625d2d2a63078bb671aa
urosh <uros.damnjanovic@gmail.com>
2014-11-04 15:12:03 +0200

    Refactoring map controller

--- app/scripts/map/map.js
+++ app/scripts/map/map.js
@@ -5,9 +5,9 @@
   angular.module('app.map')
     .controller('mapController', MapController);
 
-  MapController.$inject = ['DataModel', '$scope', '$filter', 'display', 'requestNotificationChannel'];
+  MapController.$inject = ['DataModel', '$scope', '$filter', 'display', 'requestNotificationChannel', 'mapService'];
 
-  function MapController(DataModel, $scope, $filter, display, requestNotificationChannel) {
+  function MapController(DataModel, $scope, $filter, display, requestNotificationChannel, mapService) {
     var vm = this;
     vm.markers = [];
     vm.map = {
@@ -18,106 +18,27 @@
       zoom: 8
     };
 
-    var id = 0;
 
+    activate();
 
+    function activate() {
+      vm.markers = mapService.setMarkers();
+      mapService.showMarkers(vm.markers);
 
-    setMarkers();
-    showMarkers();
+      requestNotificationChannel.onSearchResultsReady($scope, function(){
+        vm.markers = mapService.setMarkers();
+        mapService.showMarkers(vm.markers);
 
-    requestNotificationChannel.onSearchResultsReady($scope, function(){
-      setMarkers();
-      showMarkers();
+      });
 
-    });
-
-    requestNotificationChannel.onDisplayReady($scope, function(item){
-      if(item === 'filter'){
-        resetMarkers();
-      }
-    });
-
-    function setMarkers(){
-      var res = DataModel.getResults();
-
-      vm.markers = [];
-
-      for(var key in res){
-        if(res[key].lat && res[key].lng && res[key].lat!== '0'){
-          var add = true;
-          for(var i in vm.markers){
-            if( vm.markers[i].latitude === res[key].lat && vm.markers[i].longitude === res[key].lng ) {
-
-              add = false;
-            }
-          }
-
-          if(add){
-            id++;
-            vm.markers.push(
-              {
-                id: id,
-                latitude: res[key].lat,
-                longitude: res[key].lng,
-                clicked: false
-
-              }
-            );
-          }
-
+      requestNotificationChannel.onDisplayReady($scope, function(item){
+        if(item === 'filter'){
+          mapService.resetMarkers();
         }
-      }
-
-    }
-
-    function resetMarkers() {
-      _.each(vm.markers, function(marker){
-        marker.clicked = false;
-        marker.icon = null;
       });
+
     }
-
-    function showMarkers() {
-      _.each(vm.markers, function(marker){
-        marker.onClicked = function(){
-          if(marker.clicked){
-            marker.icon=null;
-            marker.clicked = false;
-            display.resetDisplay();
-
-          }else{
-            _.each(vm.markers, function(thatMarker){
-              if( thatMarker!== marker && thatMarker.icon ){
-                thatMarker.icon = null;
-                thatMarker.clicked = false;
-              }
-            });
-            marker.icon = 'images/green.png';
-            marker.clicked = true;
-            display.addDisplayData($filter('filter')(DataModel.getResults(), function(item){
-
-              if(item.lat === marker.latitude && item.lng === marker.longitude){
-                return true;
-              }else{
-                return false;
-              }
-            }), 'map');
-
-
-          }
-        };
-      });
-    }
-
-
   }
-
-
-
-
-
-
-
 
 
 })();
---
+++ app/scripts/map/map.service.js
@@ -0,0 +1,93 @@
+/**
+ * Created by urosdamnjanovic on 11/4/14.
+ */
+(function(){
+  'use strict';
+
+  angular.module('app.map')
+    .factory('mapService', MapService);
+
+  MapService.$inject = ['DataModel', 'display', '$filter'];
+
+  function MapService(DataModel, display, $filter) {
+    var service = {
+      setMarkers : setMarkers,
+      showMarkers : showMarkers,
+      resetMarkers : resetMarkers
+    };
+    var id  = 0;
+    function setMarkers() {
+
+      var markers = [];
+      var res = DataModel.getResults();
+      for(var key in res){
+        if(res[key].lat && res[key].lng && res[key].lat!== '0'){
+          var add = true;
+          for(var i in markers){
+            if( markers[i].latitude === res[key].lat && markers[i].longitude === res[key].lng ) {
+              add = false;
+            }
+          }
+
+          if(add){
+            id++;
+            markers.push(
+              {
+                id: id,
+                latitude: res[key].lat,
+                longitude: res[key].lng,
+                clicked: false
+
+              }
+            );
+          }
+
+        }
+      }
+      return markers;
+    }
+
+
+    function showMarkers(markers) {
+      _.each(markers, function(marker){
+        marker.onClicked = function(){
+          if(marker.clicked){
+            marker.icon=null;
+            marker.clicked = false;
+            display.resetDisplay();
+
+          }else{
+            _.each(markers, function(thatMarker){
+              if( thatMarker!== marker && thatMarker.icon ){
+                thatMarker.icon = null;
+                thatMarker.clicked = false;
+              }
+            });
+            marker.icon = 'images/green.png';
+            marker.clicked = true;
+            display.addDisplayData($filter('filter')(DataModel.getResults(), function(item){
+
+              if(item.lat === marker.latitude && item.lng === marker.longitude){
+                return true;
+              }else{
+                return false;
+              }
+            }), 'map');
+
+
+          }
+        };
+      });
+    }
+
+    function resetMarkers(markers) {
+      _.each(markers, function(marker){
+        marker.clicked = false;
+        marker.icon = null;
+      });
+    }
+    return service;
+
+
+  }
+})()
