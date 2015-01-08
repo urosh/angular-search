50bd79d5a40ff440554a4a5b2ddb493888159b63
urosh <uros.damnjanovic@gmail.com>
2015-01-08 16:00:20 +0200

    Changing build destination

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function (grunt) {
   // Configurable paths for the application
   var appConfig = {
     app: require('./bower.json').appPath || 'app',
-    dist: '/Applications/MAMP/htdocs/dist2'
+    dist: 'dist'
   };
 
   // Define the configuration for all the tasks
